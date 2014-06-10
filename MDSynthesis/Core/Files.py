3f7fecc341781b23c3df921f300006dd5d8e24b4
David Dotson <dldotson@asu.edu>
2014-06-10 15:22:15 -0700

    Hammering away at file locking.
    We need operations to be atomic on all files MDS works on, and this requires
    reliable file locking. After plenty of digging, the most platform-agnostic but
    generally reliable method for locking a file before a read/write operation is
    to make a symlink to it. This is because making a symlink also serves as a
    check that a symlink is already present, and because making symlinks is an
    atomic operation even on NFS mounts (as of version 2).
    
    I am implementing two types of locks: shared locks (*.shlock) and exclusive
    locks (*.exlock). A shared lock should be used before a File object reads its
    associated file, as it is a signal to other instances that it is okay to read
    but not write to that file. An exclusive lock should be used before a File
    object writes, as it signals that no read or write operations of other
    instances should take place.
    
    Examples:
        There is a call to get the name of a Sim object. This call goes to the
        Metadata Aggregator, and the File object is asked for it. The File object
        applies a shared lock on the file, reads it, then gives back the requested
        name. If a shared lock is already present, it continues anyway...
    
    THERE IS A PROBLEM WITH THIS APPROACH. First, there is no way to atomically
    check for either a shared lock or an exclusive lock at the same time. Second,
    there is no way to ensure that a shared lock will be in place so long as at
    least one reader that requires it is present, since the first to finish or
    the original maker of the lock will remove it. It is a problem if instances are
    reading the file and the lock is gone.
    
    We are reverting to the simpler but less efficient single-lock mechanism. Only
    one instance at a time gets to read or write to a file.
    
    Also, after some experimentation, we will need to open and close file streams
    at use. I was hoping we could get away with keeping the filestream for a File
    object open using
    
        filestream = open('filename', 'r+', 1)
    
    which gives line buffering and read/write mode, but though this works great on
    a single system, under NFS there is significant delay between when one host
    writes and when another host sees the changes. When the filestream is closed,
    changes appear immediately. This is probably due to how NFS handles open
    filestreams.
    
    Even though doing only one type of lock and having to open and close streams on
    each use makes the whole scheme slower, it does make it *simpler*. It will
    almost certainly be robust, barring any new potential showstoppers.

--- MDSynthesis/Core/Files.py
+++ MDSynthesis/Core/Files.py
@@ -7,15 +7,29 @@ import Aggregators
 import Workers
 
 class File(object):
-    """File object class. Implements needed file locking and syncronization.
+    """File object class. Implements file locking and syncronization.
 
     """
-    def __init__(self, *args, **kwargs):
-        """Create File instance for loading files and consistently handling locking.
+    def __init__(self, filename):
+        """Create File instance for interacting with file on disk.
+
+        The File object keeps its own cached version of a data structure
+        written to file in sync with the file itself. It does this by
+        consistently applying locks to files before reading and writing. 
+        
+        At all times, reading and modifying the data structure is the same as
+        reading and writing to the file. Accessing an element of the data
+        structure results in locking, reading, and unlocking the file each
+        time. Modifying an element results in locking, reading, writing, and
+        unlocking. All operations are performed atomically to ensure unintended
+        overwrites are avoided.
 
         """
-        self.file = open(*args, **kwargs)
-        self.lockname = "{}.lock".format(self.file.name) 
+        self.filename = filename
+        self.data = self.read(self.filename)
+
+        self.exLockname = "{}.exlock".format(self.file.name) 
+        self.shLockname = "{}.shlock".format(self.file.name) 
         self.lock()
 
     def __enter__(self):
@@ -24,13 +38,22 @@ class File(object):
     def __exit__(self, *args):
         self.close()
 
-    def lock(self):
-        """Get lock.
+    def exLock(self):
+        """Get exclusive lock on file.
+
+        The lock is just a symlink of the target file, since making a symlink
+        appears to be an atomic operation on most platforms. This is important,
+        since creating a symlink also serves to check if one is already present
+        all in one operation.
     
         """
         # if lockfile already present, wait until it disappears
-        while os.path.exists(self.lockname):
-            time.sleep(5)
+        while True:
+            try:
+                os.symlink(self.filename, self.lockname):
+                break
+            except OSError:
+                time.sleep(2)
 
         # open lockfile (will appear in filesystem)
         self.lockfile = open(self.lockname, 'w')
