1c6476f2e14234eab33f5059b9957ed627d8f9b5
David Dotson <dldotson@asu.edu>
2014-06-10 17:46:12 -0700

    Almost finished with File base class.
    The File base class contains all functionality required by other File objects
    to read and write to their persistent forms on disk with reliable file locking.
    It is flexible in that it does not care how the data structure it is
    responsible for in memory is translated to disk. This is specified by the
    functions self.reader and self.writer, which are to be defined by individual
    child classes according to their needs.
    
    This agnosticism is particularly exciting with regard to Data files, since we
    should like to make this work for ANY convenient data storage format.

--- MDSynthesis/Core/Files.py
+++ MDSynthesis/Core/Files.py
@@ -10,7 +10,7 @@ class File(object):
     """File object class. Implements file locking and syncronization.
 
     """
-    def __init__(self, filename):
+    def __init__(self, filename, reader, writer):
         """Create File instance for interacting with file on disk.
 
         The File object keeps its own cached version of a data structure
@@ -24,47 +24,110 @@ class File(object):
         unlocking. All operations are performed atomically to ensure unintended
         overwrites are avoided.
 
+        :Arguments:
+           *filename*
+              name of file on disk object synchronizes with
+           *reader*
+              function used to translate file into data structure
+           *writer*
+              function used to tranlate data structure into file
+
         """
         self.filename = filename
-        self.data = self.read(self.filename)
+        self.lockname = "{}.lock".format(self.file.name) 
 
-        self.exLockname = "{}.exlock".format(self.file.name) 
-        self.shLockname = "{}.shlock".format(self.file.name) 
-        self.lock()
+        self.reader = reader
+        self.writer = writer
 
-    def __enter__(self):
-        return self.file
+        self.read(self.filename)
 
-    def __exit__(self, *args):
-        self.close()
+    def read(self):
+        """Read contents of file into python representation.
 
-    def exLock(self):
+        :Returns:
+           *success*
+              True if write successful
+        """
+        # keep attempting lock until successful (failsafe)
+        while not self.lock():
+            continue
+
+        # keep reading until unlock gives success (only if synchronized)
+        while not self.unlock()
+            with open(self.filename, 'r') as f:
+                self.data = self.reader(f)
+        
+        return True
+
+    def write(self, writer):
+        """Write python representation to file.
+    
+        :Returns:
+           *success*
+              True if write successful
+        """
+        # keep attempting lock until successful (failsafe)
+        while not self.lock():
+            continue
+
+        # keep writing until unlock gives success (only if synchronized)
+        while not self.unlock()
+            with open(self.filename, 'w') as f:
+                self.data = self.writer(self.data)
+
+        return True
+
+    def lock(self):
         """Get exclusive lock on file.
 
         The lock is just a symlink of the target file, since making a symlink
         appears to be an atomic operation on most platforms. This is important,
         since creating a symlink also serves to check if one is already present
         all in one operation.
-    
+
+        :Returns:
+           *success*
+              True if lockfile successfully created
         """
-        # if lockfile already present, wait until it disappears
+        # if lockfile already present, wait until it disappears; make lock
         while True:
             try:
                 os.symlink(self.filename, self.lockname):
                 break
             except OSError:
-                time.sleep(2)
+                time.sleep(1)
 
-        # open lockfile (will appear in filesystem)
-        self.lockfile = open(self.lockname, 'w')
+        # return lockfile existence
+        return os.path.exists(self.lockname)
 
-    def close(self):
-        """Close file stream.
-        
+    def unlock(self):
+        """Remove exclusive lock on file.
+
+        Before removing the lock, checks that the python representation is
+        the same as what is on file.
+
+        :Returns:
+           *success*
+              True if comparison successful 
         """
-        self.file.close()
-        self.lockfile.close()
-        os.remove(self.lockname)
+        # check that python representation matches file
+        success = self.compare()
+
+        if success:
+            os.remove(self.lockname)
+
+        return success
+
+    def compare(self, reader):
+        """Compare data structure with file contents.
+
+        :Returns:
+           *same*
+              True if file synchronized with data structure
+        """
+        datatemp = 
+
+        
 
 class ContainerFile(File):
     """Container file object; syncronized access to Container data.
