10fc97f892813b051e4bfb4798fa3b9914435d91
David Dotson <dotsdl@gmail.com>
2014-06-22 03:41:21 -0700

    Removing multiple File tests for now.
    Too complicated at present to test multiple readers/writers at once.
    For now we will focus on testing using yaml as the basis for
    persistence. Later on we can build in mechanisms for testing other
    types.

--- testsuite/MDSynthesisTests/test_Core_Files.py
+++ testsuite/MDSynthesisTests/test_Core_Files.py
@@ -6,8 +6,7 @@ Tests for File classes.
 import unittest
 import MDSynthesis.Core.Files
 import yaml
-import pickle as pkl
-import json
+import os
 
 def suite():
     """Gather all tests in this module into a test suite.
@@ -26,6 +25,7 @@ class TestFile(unittest.TestCase):
         """Create test File objects. 
             
         We wish to test using multiple readers and writers at the same time. 
+        For now we just test with yaml.
 
         """
 
@@ -35,39 +35,27 @@ class TestFile(unittest.TestCase):
                            'tags': ['little', 'big'],
                            'number': 256}
         
-        self.testfile_yaml = MDSynthesis.Core.Files.File(filename='testsite/TestFile.yaml',
-                                reader=yaml.load, writer=yaml.dump, datastruct=self.datastruct}
+        self.testfile = MDSynthesis.Core.Files.File(filename='testsite/TestFile.yaml',
+                             reader=yaml.load, writer=yaml.dump, datastruct=self.datastruct}
         
-        self.logger_yaml = logging.getLogger('TestFileYaml')
-        logfile = 'testsite/TestFileYaml.log'
+        self.logger = logging.getLogger('TestFile')
+        logfile = 'testsite/TestFile.log'
         fh = logging.FileHandler(logfile)
-        self.logger_yaml.addHandler(fh)
+        self.logger.addHandler(fh)
 
+    def tearDown(self):
+        """
 
-        self.testfile_pkl = MDSynthesis.Core.Files.File(filename='testsite/TestFile.pkl',
-                                reader=pkl.load, writer=pkl.dump, datastruct=self.datastruct}
-
-        self.logger_pkl = logging.getLogger('TestFilePkl')
-        logfile = 'testsite/TestFilePkl.log'
-        fh = logging.FileHandler(logfile)
-        self.logger_pkl.addHandler(fh)
-
-
-        self.testfile_json = MDSynthesis.Core.Files.File(filename='testsite/TestFile.json',
-                                reader=json.load, writer=json.dump, datastruct=self.datastruct}
-
-        self.logger_json = logging.getLogger('TestFileJson')
-        logfile = 'testsite/TestFileJson.log'
-        fh = logging.FileHandler(logfile)
-        self.logger_json.addHandler(fh)
+        """
 
 
     def test_lock(self):
         """Test the basic locking mechanism of the File class.
 
         """
-        pass
-        
+        self.testfile.lock()
+        self.assertTrue(os.path.exists(self.testfile.lockname))
+
 
 if __name__ == '__main__':
     unittest.main()
