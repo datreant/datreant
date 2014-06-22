ab8a1b56a5227f803a24fb1fbe020594bfd785c9
David Dotson <dotsdl@gmail.com>
2014-06-22 02:28:59 -0700

    Added mechanisms for running all tests in module.
    Mostly getting this from StackOverflow. I'm not certain calling
    unittest.TextTestRunner.run() is the way to go to run all tests, or if
    we should instead use unittest.main() with some default arguments. We'll
    see when I try using it.

--- testsuite/MDSynthesisTests/__init__.py
+++ testsuite/MDSynthesisTests/__init__.py
@@ -4,5 +4,21 @@ Unit tests for MDSynthesis
 """
 
 import unittest
+import test_Core_Files
 
 
+def suite():
+    """Gather every test from package into a test suite.
+
+    """
+    test_suite = unittest.TestSuite()
+    test_suite.addTest(test_Core_Files.suite())
+
+    return test_suite
+
+
+if __name__ == '__main__':
+    #so we can run tests from this package individually
+    test_runner = unittest.TextTestRunner()
+    test_suite = suite()
+    test_runner.run(test_suite)
--- testsuite/MDSynthesisTests/test_Core_Files.py
+++ testsuite/MDSynthesisTests/test_Core_Files.py
@@ -3,6 +3,17 @@ Tests for File classes.
 
 """
 
+import unittest
+
+def suite():
+    """Gather all tests in this module into a test suite.
+
+    """
+    test_suite = unittest.TestSuite()
+    test_suite.addTest(unittest.TestLoader().loadTestsFromTestCase(TestFile))
+
+    return test_suite
+
 class TestFile(unittest.TestCase):
     """Test class for the File class.
 
