d88b5a5f8ac0d2af686484b9de5e2d702fb10297
David Dotson <dotsdl@gmail.com>
2014-11-10 22:28:55 -0700

    Removed most of ContainerCore.
    Most of ContainerCore's functionality was IO and metadata formatting,
    which is now handled by the underlying File classes. As such, all that
    remains really is logging. The _start_logger method needs some work to
    make it consistent with new accessors.

--- MDSynthesis/Core/Workers.py
+++ MDSynthesis/Core/Workers.py
@@ -31,24 +31,67 @@ class Finder(object):
 
     """
 
+    def _locate_database(self, **kwargs):
+        """Find database; to be used if it can't be found.
+
+        The Container looks upward from its location on the filesystem through
+        the file heirarchy, looking for a Database file. The directory containing
+        the first such file found will be returned. None is returned if no such
+        files found.
+
+        :Keywords:
+            *startdir*
+                directory from which to begin upward search; default is
+                Container basedir
+
+        :Returns:
+            *database*
+                directory of located Database; if no Database found, is None
+        
+        """
+        startdir = kwargs.pop('startdir', None)
+        
+        if not startdir:
+            startdir = self.metadata['basedir']
+
+        # search upward for a database
+        startdir = os.path.abspath(startdir)
+        directory = startdir
+        found = False
+        
+        self._logger.info("Beginning search for database from {}".format(directory))
+
+        while (directory != '/') and (not found):
+            directory, tail = os.path.split(directory)
+            candidates = glob.glob(os.path.join(directory, self._databasefile))
+            
+            if candidates:
+                self._logger.info("Database candidate located: {}".format(candidates[0]))
+                basedir = os.path.dirname(candidates[0])
+                db = Database.Database(basedir)
+                found = db._handshake()
+        
+        if not found:
+            self._logger.warning("No database found!")
+            basedir = None
+
+        return basedir
+
 class Utilities(object):
     """Lowest level utilities; contains all methods that are common to every 
        MDSynthesis object.
 
     """
-    def open(self, *args, **kwargs):
-        """Open a file for i/o and apply an exclusive lock.
-    
-        Arguments and keywords are passed directly to the open() builtin.
-    
-        """
-        F = Files.File(*args, **kwargs)
-        return F
-    
     def makedirs(self, p):
         if not os.path.exists(p):
             os.makedirs(p)
     
+    def generate_uuid(self):
+        """Generate a 'unique' identifier.
+
+        """
+        return str(uuid4())
+
 class Attributes(object):
     """Class for user-defined attributes.
 
