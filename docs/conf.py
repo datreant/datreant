86e15155a62af7221f8f1f8e491594fb865612c0
David Dotson <dotsdl@gmail.com>
2015-01-05 00:19:20 -0700

    Added mock modules to conf.
    Supposedly readthedocs can work with this, since we can't build these
    dependencies there (they need some C libraries). Trying it out.

--- docs/conf.py
+++ docs/conf.py
@@ -277,4 +277,16 @@ if not on_rtd:  # only import and set the theme if we're building docs locally
 
 # otherwise, readthedocs.org uses their theme by default, so no need to specify it
 
+# get docstrings for init methods, too
 autoclass_content = 'both'
+
+# for readthedocs, since it cannot compile dependencies that require C libraries
+from mock import Mock as MagicMock
+
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+            return Mock()
+
+MOCK_MODULES = ['pandas', 'tables', 'h5py', 'MDAnalysis']
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
