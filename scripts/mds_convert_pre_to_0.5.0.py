7fd4873f72cef21e7470abc2232c6f7f04d2924e
David Dotson <dotsdl@gmail.com>
2015-03-17 22:13:59 -0700

    Renamed state file conversion script; added additional docs.
    This script should be used on state files that were made before the
    0.5.0 release. It is safe to use on state files that were created after,
    as well as those the script has already been run on. It only changes the
    state file name using the uuid stored inside.

--- scripts/mds_convert_pre_to_0.5.0.py
+++ scripts/mds_convert_pre_to_0.5.0.py
@@ -1,5 +1,12 @@
 #!/usr/bin/env python2
 
+"""This script will convert state files from pre-release format to 0.5.0 format.
+   
+The only change is in state file naming: the uuid of the file is included in
+the filename.
+
+"""
+
 import mdsynthesis as mds
 import os
 import tables
@@ -11,8 +18,10 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     for statefile in args.statefile:
+
         f = tables.open_file(statefile, 'r')
         table = f.get_node('/', 'meta')
+
         containertype = table.cols.containertype[0]
         uuid = table.cols.uuid[0]
         f.close()
