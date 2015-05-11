889e9f9853001aae7959e2d8dc8807233aa7c21f
David Dotson <dotsdl@gmail.com>
2015-05-11 16:37:12 -0700

    Addressed PEP8 violations in a number of files.
    We now have py.test use the pep8 plugin to flag areas where code doesn't
    follow a consistent style, and this has pointed to a host of violations.
    Most of these are either::
    
        * line too long
        * continuation at inconsistent indent level
        * whitespace on blank line
    
    I addressed these in a number of files, but there are stil more to go.
    It's mostly just tedious work, but should be done to help ensure future
    code that is merged into master is readable and has consistent style.

--- mdsynthesis/__init__.py
+++ mdsynthesis/__init__.py
@@ -13,20 +13,18 @@ dissect trajectories, MDSynthesis provides a framework for automatically
 organizing the results. This allows you (the scientist) to focus on your
 science, letting the computer handle the lower-level logistical details.
 
-.. SeeAlso:: :class:`mdsynthesis.containers.Sim` 
+.. SeeAlso:: :class:`mdsynthesis.containers.Sim`
              :class:`mdsynthesis.containers.Group`
 
 
 """
-
-__version__ = "0.4.0-dev"  # NOTE: keep in sync with RELEASE in setup.py
-
-__all__ = ['Sim', 'Group', 'Coordinator']
-
 # Bring some often used objects into the current namespace
-#from coordinator import Coordinator
 from containers import Sim, Group
 from coordinator import Coordinator
 import core
 from core.bundle import Bundle
 from manipulators import *
+
+__version__ = "0.4.0-dev"  # NOTE: keep in sync with RELEASE in setup.py
+
+__all__ = ['Sim', 'Group', 'Coordinator']
--- mdsynthesis/coordinator.py
+++ mdsynthesis/coordinator.py
@@ -1,12 +1,14 @@
 """
-The Coordinator object stores information on all Containers it is made aware of.
-This centralized storage allows Containers to find each other more easily when
-necessary; this is especially important for Groups. It also provides an interface
-for querying the whole set of Containers it is aware of and returning them;
-this can remove the need to manually go hunting for Containers in the filesystem.
+The Coordinator object stores information on all Containers it is made aware
+of.  This centralized storage allows Containers to find each other more easily
+when necessary; this is especially important for Groups. It also provides an
+interface for querying the whole set of Containers it is aware of and returning
+them; this can remove the need to manually go hunting for Containers in the
+filesystem.
 
 """
 import core
+
 
 class Coordinator(object):
     """Macrocontroller for all Containers in the database.
@@ -24,14 +26,12 @@ class Coordinator(object):
         """
 
         # initialize aggregators
-        #self.add =
-        #self.remove = 
-        #self.move = 
+        # self.add =
+        # self.remove =
+        # self.move =
         pass
 
     def select(self):
         """
         """
         pass
-
-    
