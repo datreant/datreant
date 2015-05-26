6cc865cb87258f77a727d533f67de75ac1c9cd09
David Dotson <dotsdl@gmail.com>
2015-05-25 22:45:28 -0700

    Added scandir to mocked modules for building the docs.
    Autodoc requires faking modules that need to be compiled if we are to
    let readthedocs handle doc building. Scandir doesn't require compiling
    IIRC, but it's easier to fake than to bother messing with the install
    dependencies for just the docs.

--- docs/conf.py
+++ docs/conf.py
@@ -288,5 +288,5 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
             return Mock()
 
-MOCK_MODULES = ['pandas', 'tables', 'h5py', 'MDAnalysis']
+MOCK_MODULES = ['pandas', 'tables', 'h5py', 'MDAnalysis', 'scandir']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
