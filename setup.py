ab087be84df47c4e50340b2c23947ad2076b87d0
David Dotson <dotsdl@gmail.com>
2015-09-09 13:50:22 -0700

    Revert.

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ For a basic installation just type the command::
 from setuptools import setup
 
 setup(name='datreant',
-      version='0.6.0dev',
+      version='0.6.0-dev',
       author='David Dotson',
       author_email='dotsdl@gmail.com',
       packages=['datreant', 'datreant.tests'],
