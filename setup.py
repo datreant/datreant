2704a4b4dd8750a30bfcc8577cce5df2c36912fd
David Dotson <dotsdl@gmail.com>
2015-09-09 13:49:09 -0700

    Bleh...trying to get mdsynthesis' setup.py to pull datreant sources.

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ For a basic installation just type the command::
 from setuptools import setup
 
 setup(name='datreant',
-      version='0.6.0-dev',
+      version='0.6.0dev',
       author='David Dotson',
       author_email='dotsdl@gmail.com',
       packages=['datreant', 'datreant.tests'],
