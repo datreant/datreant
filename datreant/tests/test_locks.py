9f392096d7efbf239a3c48b61b0f67d132b4563d
David Dotson <dotsdl@gmail.com>
2015-12-19 15:29:43 -0700

    Locks work for both backends(!).
    Now we have a very human-readable format for state files. Also it has no
    hdf5 dependency. I think this can lead to great things. :D

--- datreant/tests/test_locks.py
+++ datreant/tests/test_locks.py
@@ -13,59 +13,53 @@ import pandas as pd
 import datreant as dtr
 
 from datreant.backends.pytables import TreantFile
+from datreant.backends.pyyaml import yamlTreantFile
 
 
-class Locks:
-    @pytest.fixture
-    def treant(self, tmpdir):
-        with tmpdir.as_cwd():
-            t = dtr.treants.Treant('sprout')
-        return t
+def pokefile(treantfileclass, treantfilepath, string):
+    """Add a number of tags directly to a TreantFile."""
+    treantfile = treantfileclass(treantfilepath)
+    treantfile.add_tags(*["{}_{}".format(string, i) for i in range(100)])
+
+
+def append(backend, treantfilepath, df):
+    treant = dtr.Treant(treantfilepath, backend=backend)
+    treant.data.append('testdata', df)
+   
+
+def init_treant(backend, tmpdir, tags):
+    with tmpdir.as_cwd():
+        tf = dtr.Treant('sprout', tags=tags, backend=backend)
+    return tf
+    
+
+class LocksMixin:
     
     @pytest.fixture
     def dataframe(self):
         data = np.random.rand(100, 3)
         return pd.DataFrame(data, columns=('A', 'B', 'C'))
     
-    @pytest.fixture
-    def treantfile(self, tmpdir):
-        with tmpdir.as_cwd():
-            tf = TreantFile('testtreantfile.h5')
-        return tf
     
-    def pokefile(self, treantfilepath, string):
-        """Add a number of tags directly to a TreantFile."""
-        treantfile = TreantFile(treantfilepath)
-        treantfile.add_tags(*["{}_{}".format(string, i) for i in range(1000)])
-    
-    def test_death_by_99000_pokes(self, treantfile):
+    def test_death_by_1000_pokes(self, treantfile):
         pool = mp.Pool(processes=4)
-        for i in range(99):
-            pool.apply_async(self.pokefile, args=(treantfile.filename,
+        for i in range(10):
+            pool.apply_async(pokefile, args=(self.treantfileclass, treantfile.filename,
                                              "run_{}".format(i)))
         pool.close()
         pool.join()
     
-        assert len(treantfile.get_tags()) == 99000
-    
-    def append(self, treantfilepath, df):
-        treant = dtr.Treant(treantfilepath)
-        treant.data.append('testdata', df)
+        assert len(treantfile.get_tags()) == 1000
     
     def test_async_append(self, treant, dataframe):
         pool = mp.Pool(processes=4)
         num = 53
         for i in range(num):
-            pool.apply_async(self.append, args=(treant.filepath, dataframe))
+            pool.apply_async(append, args=(self.backend, treant.filepath, dataframe))
         pool.close()
         pool.join()
     
         assert len(treant.data['testdata']) == len(dataframe)*(num+0)
-    
-    def init_treant(self, tmpdir, tags):
-        with tmpdir.as_cwd():
-            tf = dtr.Treant('sprout', tags=tags)
-        return tf
     
     def test_init_treant(self, tmpdir):
         pool = mp.Pool(processes=4)
@@ -73,9 +67,11 @@ class Locks:
     
         # TODO: eventually want this to work without initing the treant
         # here
-        self.init_treant(tmpdir, ['bark'])
+        init_treant(self.backend, tmpdir, ['bark'])
         for i in range(num):
-            pool.apply_async(self.init_treant, args=(tmpdir, ['run_{}'.format(i)]))
+            pool.apply_async(init_treant, args=(self.backend,
+                                                tmpdir,
+                                                ['run_{}'.format(i)]))
         pool.close()
         pool.join()
     
@@ -85,18 +81,35 @@ class Locks:
         assert len(tf.tags) == num + 1
 
 
-class TestPytables(Locks):
+class TestPytables(LocksMixin):
+    backend = 'pytables'
+    treantfileclass = TreantFile
+
     @pytest.fixture
     def treant(self, tmpdir):
         with tmpdir.as_cwd():
             t = dtr.treants.Treant('sprout', backend='pytables')
         return t
+
+    @pytest.fixture
+    def treantfile(self, tmpdir):
+        with tmpdir.as_cwd():
+            tf = TreantFile('testtreantfile.h5')
+        return tf
     
 
-class TestYaml(Locks):
+class TestYaml(LocksMixin):
+    backend = 'pyyaml'
+    treantfileclass = yamlTreantFile
+
     @pytest.fixture
     def treant(self, tmpdir):
         with tmpdir.as_cwd():
-            t = dtr.treants.Treant('sprout', backend='yaml')
+            t = dtr.treants.Treant('sprout', backend='pyyaml')
         return t
     
+    @pytest.fixture
+    def treantfile(self, tmpdir):
+        with tmpdir.as_cwd():
+            tf = yamlTreantFile('testtreantfile.yml')
+        return tf
