590cb79e02d8d8056c7dc5a421c13cafb8a3c149
David Dotson <dotsdl@gmail.com>
2016-03-11 12:25:31 -0700

    Forgot to re-add changed test file name.

---
+++ src/datreant/core/tests/test_collections.py
@@ -0,0 +1,279 @@
+"""Tests for Bundle.
+
+"""
+
+import pytest
+
+import datreant.core as dtr
+
+
+def do_stuff(cont):
+    return cont.name + cont.uuid
+
+
+def return_nothing(cont):
+    b = cont.name + cont.uuid
+
+
+class TestBundle():
+    """Tests for common elements of Group.members and Bundle"""
+
+    @pytest.fixture
+    def collection(self):
+        return dtr.Bundle()
+
+    @pytest.fixture
+    def testtreant(self, tmpdir, request):
+        with tmpdir.as_cwd():
+            t = dtr.Treant('dummytreant')
+        return t
+
+    @pytest.fixture
+    def testgroup(self, tmpdir, request):
+        with tmpdir.as_cwd():
+            g = dtr.Group('dummygroup')
+            g.members.add(dtr.Treant('bark'), dtr.Treant('leaf'))
+        return g
+
+    def test_additive(self, tmpdir, testtreant, testgroup, collection):
+        """Test that addition of treants and collections give Bundles.
+
+        """
+        with tmpdir.as_cwd():
+            assert isinstance(testtreant + testgroup, dtr.Bundle)
+            assert len(testtreant + testgroup) == 2
+
+            # subtle, but important; Group.members is a collection,
+            # while Group is a treant
+            assert len(testtreant + testgroup.members) != 2
+            assert (len(testtreant + testgroup.members) ==
+                    len(testgroup.members) + 1)
+
+            assert isinstance(testtreant + testgroup.members, dtr.Bundle)
+
+            b = collection + testtreant + testgroup
+
+            # beating a dead horse
+            assert len(b) == 2
+            assert (len(b + testgroup.members) ==
+                    len(b) + len(testgroup.members))
+            assert isinstance(b + testgroup.members, dtr.Bundle)
+
+    def test_subset(self, collection):
+        pass
+
+    def test_superset(self, collection):
+        pass
+
+    def test_difference(self, collection):
+        pass
+
+    def test_symmetric_difference(self, collection):
+        pass
+
+    def test_union(self, collection):
+        pass
+
+    def test_intersection(self, collection):
+        pass
+
+    def test_intersection(self, collection):
+        pass
+
+    def test_add_members(self, collection, tmpdir):
+        """Try adding members in a number of ways"""
+        with tmpdir.as_cwd():
+            s1 = dtr.Treant('lark')
+            s2 = dtr.Treant('hark')
+            g3 = dtr.Group('linus')
+
+            collection.add(s1, [g3, s2])
+
+            for cont in (s1, s2, g3):
+                assert cont in collection
+
+            s4 = dtr.Treant('snoopy')
+            collection.add([[s4], s2])
+            assert s4 in collection
+
+            # the group won't add members it alrady has
+            # (operates as an ordered set)
+            assert len(collection) == 4
+
+    def test_add_members_glob(self, collection, tmpdir):
+        """Try adding members with globbing"""
+        with tmpdir.as_cwd():
+            t1 = dtr.Treant('lark')
+            t2 = dtr.Treant('hark')
+            g3 = dtr.Group('linus')
+
+            collection.add('*ark')
+
+            for treant in (t1, t2):
+                assert treant in collection
+
+            assert g3 not in collection
+
+    def test_get_members(self, collection, tmpdir):
+        """Access members with indexing and slicing"""
+        with tmpdir.as_cwd():
+            s1 = dtr.Treant('larry')
+            g2 = dtr.Group('curly')
+            s3 = dtr.Treant('moe')
+
+            collection.add([[[s1, [g2, [s3]]]]])
+
+            assert collection[1] == g2
+
+            c4 = dtr.treants.Treant('shemp')
+            collection.add(c4)
+
+            for member in (s1, g2, s3):
+                assert member in collection[:3]
+
+            assert c4 not in collection[:3]
+            assert c4 == collection[-1]
+
+    def test_fancy_index(self, collection):
+        pass
+
+    def test_name_index(self, collection):
+        pass
+
+    def test_uuid_index(self, collection):
+        pass
+
+    def test_remove_members(self, collection, tmpdir):
+        """Try removing members"""
+        with tmpdir.as_cwd():
+            g1 = dtr.Group('lion-o')
+            s2 = dtr.Treant('cheetara')
+            s3 = dtr.Treant('snarf')
+
+            collection.add(s3, g1, s2)
+
+            for cont in (g1, s2, s3):
+                assert cont in collection
+
+            collection.remove(1)
+            assert g1 not in collection
+
+            collection.remove(s2)
+            assert s2 not in collection
+
+    def test_remove_members_name(self, collection, tmpdir):
+        """Try removing members with names and globbing"""
+        with tmpdir.as_cwd():
+            t1 = dtr.Treant('lark')
+            t2 = dtr.Treant('elsewhere/lark')
+            t3 = dtr.Treant('hark')
+            g = dtr.Group('linus')
+
+            stuff = [t1, t2, t3, g]
+
+            # test removal by name
+            collection.add(stuff)
+            for item in stuff:
+                assert item in collection
+
+            # should remove both treants with name 'lark'
+            collection.remove('lark')
+
+            for item in (t3, g):
+                assert item in collection
+
+            for item in (t1, t2):
+                assert item not in collection
+
+            # test removal by a unix-style glob pattern
+            collection.add(stuff)
+            for item in stuff:
+                assert item in collection
+
+            # should remove 'lark' and 'hark' treants
+            collection.remove('*ark')
+
+            assert g in collection
+
+            for item in (t1, t2, t3):
+                assert item not in collection
+
+    def test_member_attributes(self, collection, tmpdir):
+        """Get member uuids, names, and treanttypes"""
+        with tmpdir.as_cwd():
+            c1 = dtr.treants.Treant('bigger')
+            g2 = dtr.Group('faster')
+            s3 = dtr.Treant('stronger')
+
+        collection.add(c1, g2, s3)
+
+        uuids = [cont.uuid for cont in [c1, g2, s3]]
+        assert collection.uuids == uuids
+
+        names = [cont.name for cont in [c1, g2, s3]]
+        assert collection.names == names
+
+        treanttypes = [cont.treanttype for cont in [c1, g2, s3]]
+        assert collection.treanttypes == treanttypes
+
+    def test_map(self, collection, tmpdir):
+        with tmpdir.as_cwd():
+            s1 = dtr.Treant('lark')
+            s2 = dtr.Treant('hark')
+            g3 = dtr.Group('linus')
+
+        collection.add(s1, s2, g3)
+
+        comp = [cont.name + cont.uuid for cont in collection]
+        assert collection.map(do_stuff) == comp
+        assert collection.map(do_stuff, processes=2) == comp
+
+        assert collection.map(return_nothing) is None
+        assert collection.map(return_nothing, processes=2) is None
+
+    def test_flatten(self, collection, tmpdir):
+        """Test that flattening a collection of Treants and Groups works as
+        expected.
+
+        """
+        treantnames = ('lark', 'mark', 'bark')
+        with tmpdir.as_cwd():
+            g = dtr.Group('bork')
+
+            for name in treantnames:
+                dtr.Treant(name)
+
+            g.members.add('bork', *treantnames)
+
+            # now our collection has a Group that has itself as a member
+            # the flattened collection should detect this "loop" and leave
+            # out the Group
+            collection.add(g)
+
+            assert len(collection) == 1
+
+            b = collection.flatten()
+
+            # shouldn't be any Groups
+            assert g not in b
+
+            # should have all our Treants
+            assert len(b) == 3
+            for name in treantnames:
+                assert name in b.names
+
+            # if we exclude the Group from the flattening, this should leave us
+            # with nothing
+            assert len(collection.flatten([g.uuid])) == 0
+
+            # if one of the Treants is also a member of the collection,
+            # should get something
+            collection.add('mark')
+            assert len(collection.flatten([g.uuid])) == 1
+            assert 'mark' in collection.flatten([g.uuid]).names
+
+    class TestAggTags:
+        pass
+
+    class TestAggCategories:
+        pass
