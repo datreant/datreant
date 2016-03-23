c17072eab3b5f0eb26999613dd362c70bf160bb2
David Dotson <dotsdl@gmail.com>
2016-03-23 15:05:22 -0700

    First commit of 0.7.0.dev.

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ For a basic installation just type the command::
 from setuptools import setup, find_packages
 
 setup(name='datreant.core',
-      version='0.6.0',
+      version='0.7.0.dev',
       description='persistent, pythonic trees for heterogeneous data',
       author='David Dotson',
       author_email='dotsdl@gmail.com',
