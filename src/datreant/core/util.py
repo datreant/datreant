89324bb9f4af613309a3d191f88363c054428593
Rene Dworschak <r.7dworschak@googlemail.com>
2017-02-01 23:41:46 +0100

    touch ver3

--- src/datreant/core/util.py
+++ src/datreant/core/util.py
@@ -19,9 +19,21 @@ def makedirs(path):
             raise
 
 
-def touch_me(fname, mode=0o666, dir_fd=None, **kwargs):
+def touch_me2(fname, mode=0o666, dir_fd=None, **kwargs):
     if os.name == 'nt':
         flags = os.O_CREAT | os.O_APPEND
         with os.fdopen(os.open(fname, flags=flags, mode=mode, dir_fd=dir_fd)) as filename:
             os.utime(filename.fileno() if os.utime in os.supports_fd else fname,
                      dir_fd=None if os.supports_fd else dir_fd, **kwargs)
+
+
+def touch_me(path):
+    if not path:
+        return
+    path = fullpath(path)
+    if path.find("/") > 0 and not os.path.exists(os.path.dirname(path)):
+        os.makedirs(os.path.dirname(path))
+    try:
+        os.utime(path, None)
+    except Exception:
+        open(path, 'a').close()
