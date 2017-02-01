8fdd0419944ef412bc3d719a7d71b4f6d4830944
Rene Dworschak <r.7dworschak@googlemail.com>
2017-02-01 23:02:05 +0100

    touch ver2

--- src/datreant/core/util.py
+++ src/datreant/core/util.py
@@ -1,5 +1,5 @@
 import os
-import time
+
 
 def makedirs(path):
     """Make directories and all parents necessary.
@@ -19,15 +19,9 @@ def makedirs(path):
             raise
 
 
-def touch_me(path):
+def touch_me(fname, mode=0o666, dir_fd=None, **kwargs):
     if os.name == 'nt':
-        now = time.time()
-        try:
-            # assume it's there
-            os.utime(path, (now, now))
-        except os.error:
-            # if it isn't, try creating the directory,
-            # a file with that name
-            os.makedirs(os.path.dirname(path))
-            open(path, "w").close()
-            os.utime(path, (now, now))
+        flags = os.O_CREAT | os.O_APPEND
+        with os.fdopen(os.open(fname, flags=flags, mode=mode, dir_fd=dir_fd)) as filename:
+            os.utime(filename.fileno() if os.utime in os.supports_fd else fname,
+                     dir_fd=None if os.supports_fd else dir_fd, **kwargs)
