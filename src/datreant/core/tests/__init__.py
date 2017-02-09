26743275aaa83ab06c49a50bb548d6568146edff
Rene Dworschak <r.7dworschak@googlemail.com>
2017-02-09 10:08:57 +0100

    Update __init__.py

--- src/datreant/core/tests/__init__.py
+++ src/datreant/core/tests/__init__.py
@@ -1,12 +1,12 @@
--# -*- Mode: python; tab-width: 4; indent-tabs-mode:nil; -*-		
- -# vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4		
- -#		
- -# datreant.tests		
- -		
- -"""		
- -datreant.tests --- unit tests for datreant		
- -=============================================================		
- -		
- -"""		
- -# Bring some often used objects into the current namespace		
- -from . import test_collections
+# -*- Mode: python; tab-width: 4; indent-tabs-mode:nil; -*-		
+# vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4		
+#		
+# datreant.tests		
+	
+"""		
+datreant.tests --- unit tests for datreant		
+=============================================================		
+		
+"""		
+# Bring some often used objects into the current namespace		
+from . import test_collections
