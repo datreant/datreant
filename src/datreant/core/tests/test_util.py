ea53eec231a83a5f0591b34c2f0237ef9d8c3d30
Richard Gowers <richardjgowers@gmail.com>
2017-03-15 14:25:01 +0000

    Add tests for core.util
    Mock now a requirement

---
+++ src/datreant/core/tests/test_util.py
@@ -0,0 +1,36 @@
+import mock
+import os
+import pytest
+
+import datreant.core as dtr
+
+
+class TestMakedirs(object):
+    def test_makedirs(self, tmpdir):
+        with tmpdir.as_cwd():
+            dtr.util.makedirs('this/should/exist')
+            assert os.path.exists('this/should/exist')
+            assert os.path.isdir('this/should/exist')
+
+    def test_makedirs_exists(self, tmpdir):
+        # try and make a dir twice
+        with tmpdir.as_cwd():
+            os.mkdir('this/')
+            dtr.util.makedirs('this/')
+            assert os.path.exists('this/')
+            assert os.path.isdir('this/')
+
+    def test_makedirs_error_catch(self, tmpdir):
+        # mock a disk full error
+        # and make sure it gets propagated through properly
+        with tmpdir.as_cwd():
+            with mock.patch('os.makedirs') as mp:
+                mp.side_effect = OSError(os.errno.ENOSPC, 'Mock - disk full')
+                with pytest.raises(OSError):
+                    dtr.util.makedirs('this/should/fail')
+                # check the specific error code
+                # ie check we don't mangle it enroute
+                try:
+                    dtr.util.makedirs('this/should/fail')
+                except OSError as e:
+                    assert e.errno == os.errno.ENOSPC
