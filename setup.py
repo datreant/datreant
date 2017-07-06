6f11e7dee0e71d2db2885f7f7411faede5ebb804
Max Linke <max_linke@gmx.de>
2017-07-06 10:28:27 +0200

    add selection str parser
    This adds a parser for tag selections to use
    
    b.tags['apple and ham or tuna']

--- setup.py
+++ setup.py
@@ -9,13 +9,14 @@ For a basic installation just type the command::
 
 from setuptools import setup, find_packages
 
-setup(name='datreant.core',
-      version='0.8.0-dev',
-      description='persistent, pythonic trees for heterogeneous data',
-      author='David Dotson',
-      author_email='dotsdl@gmail.com',
-      url = 'http://datreant.org/',
-      classifiers=[
+setup(
+    name='datreant.core',
+    version='0.8.0-dev',
+    description='persistent, pythonic trees for heterogeneous data',
+    author='David Dotson',
+    author_email='dotsdl@gmail.com',
+    url='http://datreant.org/',
+    classifiers=[
         'Development Status :: 4 - Beta',
         'Environment :: Console',
         'Intended Audience :: Science/Research',
@@ -24,13 +25,14 @@ setup(name='datreant.core',
         'Programming Language :: Python',
         'Topic :: Scientific/Engineering',
         'Topic :: Software Development :: Libraries :: Python Modules',
-        ],
-      packages=find_packages('src'),
-      namespace_packages=['datreant'],
-      package_dir={'': 'src'},
-      scripts=[],
-      license='BSD',
-      long_description=open('README.rst').read(),
-      tests_require = ['numpy', 'pytest>=2.10', 'mock'],
-      install_requires=['asciitree', 'pathlib2', 'scandir', 'six', 'fuzzywuzzy']
-      )
+    ],
+    packages=find_packages('src'),
+    namespace_packages=['datreant'],
+    package_dir={'': 'src'},
+    scripts=[],
+    license='BSD',
+    long_description=open('README.rst').read(),
+    tests_require=['numpy', 'pytest>=2.10', 'mock'],
+    install_requires=[
+        'asciitree', 'pathlib2', 'scandir', 'six', 'fuzzywuzzy', 'pyparsing'
+    ])
---
+++ src/datreant/core/tests/test_parser.py
@@ -0,0 +1,49 @@
+"""Parser tests for Treants.
+
+"""
+
+from datreant.core.selectionparser import parse_selection
+
+import pytest
+
+
+@pytest.mark.parametrize('expr', ('foo bar', 'foo'))
+def test_names(expr):
+    assert parse_selection(expr) == expr
+
+
+@pytest.mark.parametrize("expr, typename", (("foo and bar", list),
+                                           ("foo or bar", tuple),
+                                           ("not foo", set)))
+def test_type_operation(expr, typename):
+    sel = parse_selection(expr)
+    assert isinstance(sel, typename)
+
+
+@pytest.mark.parametrize('terms', (['foo', 'bar2'],
+                                   ['a', 'b', 'c'],
+                                   ['d', 'e', 'f', 'g']))
+def test_and(terms):
+    expr = ' and '.join(terms)
+    assert parse_selection(expr) == terms
+
+
+@pytest.mark.parametrize('terms', (('foo', 'bar2'),
+                                   ('a', 'b', 'c'),
+                                   ('d', 'e', 'f', 'g')))
+def test_or(terms):
+    expr = ' or '.join(terms)
+    assert parse_selection(expr) == terms
+
+
+@pytest.mark.parametrize('terms', (('foo', 'bar2'),
+                                   ('a', 'b', 'c'),
+                                   ('d', 'e', 'f', 'g')))
+def test_not(terms):
+    expr = 'not ({})'.format(' and '.join(terms))
+    assert parse_selection(expr) == [{el} for el in terms]
+
+
+def test_complex_expr():
+    expr = "not food or (leaves and animals)"
+    assert parse_selection(expr) == ({'food'}, ['leaves', 'animals'])
