a19006f2a0826abc144ee8dfb5a5a726a9fe37e2
Max Linke <max_linke@gmx.de>
2017-08-16 15:40:12 +0200

    Allow all printable chars in selection parser

--- src/datreant/core/tests/test_parser.py
+++ src/datreant/core/tests/test_parser.py
@@ -13,31 +13,28 @@ def test_names(expr):
 
 
 @pytest.mark.parametrize("expr, typename", (("foo and bar", list),
-                                            ("foo or bar", tuple),
-                                            ("not foo", set)))
+                                            ("foo or bar", tuple), ("not foo",
+                                                                    set)))
 def test_type_operation(expr, typename):
     sel = parse_selection(expr)
     assert isinstance(sel, typename)
 
 
-@pytest.mark.parametrize('terms', (['foo', 'bar2'],
-                                   ['a', 'b', 'c'],
+@pytest.mark.parametrize('terms', (['foo', 'bar2'], ['a', 'b', 'c'],
                                    ['d', 'e', 'f', 'g']))
 def test_and(terms):
     expr = ' and '.join(terms)
     assert parse_selection(expr) == terms
 
 
-@pytest.mark.parametrize('terms', (('foo', 'bar2'),
-                                   ('a', 'b', 'c'),
+@pytest.mark.parametrize('terms', (('foo', 'bar2'), ('a', 'b', 'c'),
                                    ('d', 'e', 'f', 'g')))
 def test_or(terms):
     expr = ' or '.join(terms)
     assert parse_selection(expr) == terms
 
 
-@pytest.mark.parametrize('terms', (('foo', 'bar2'),
-                                   ('a', 'b', 'c'),
+@pytest.mark.parametrize('terms', (('foo', 'bar2'), ('a', 'b', 'c'),
                                    ('d', 'e', 'f', 'g')))
 def test_not(terms):
     expr = 'not ({})'.format(' and '.join(terms))
@@ -47,3 +44,13 @@ def test_not(terms):
 def test_complex_expr():
     expr = "not food or (leaves and animals)"
     assert parse_selection(expr) == ({'food'}, ['leaves', 'animals'])
+
+
+@pytest.mark.parametrize(
+    'char',
+    list(
+        '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!"#$%&\'*+,-./:;<=>?@[]^_`{|}~'
+    ))
+def test_possible_characters(char):
+    sel = parse_selection("food and a{}".format(char))
+    assert isinstance(sel, list)
