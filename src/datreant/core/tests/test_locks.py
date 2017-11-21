169ae8ee2760c88c9817ce054279573e4259f2a6
Max Linke <max_linke@gmx.de>
2017-11-21 08:46:07 +0100

    clean up test_locks

--- src/datreant/core/tests/test_locks.py
+++ src/datreant/core/tests/test_locks.py
@@ -2,18 +2,33 @@
 
 """
 
-import string
 import multiprocessing as mp
-import time
 import pytest
 
 from datreant.core import Treant
 
 
+@pytest.fixture
+def treant(tmpdir):
+    with tmpdir.as_cwd():
+        t = Treant('sprout')
+    return t
+
+
 def pokefile(treantpath, string):
     """Add a number of tags to a Treant."""
     treant = Treant(treantpath)
-    treant.tags.add(*["{}_{}".format(string, i) for i in range(100)])
+    treant.tags.add(* ["{}_{}".format(string, i) for i in range(100)])
+
+
+def test_death_by_1000_pokes(treant):
+    pool = mp.Pool(processes=4)
+    for i in range(10):
+        pool.apply_async(pokefile, args=(treant.abspath, "run_{}".format(i)))
+    pool.close()
+    pool.join()
+
+    assert len(treant.tags) == 1000
 
 
 def init_treant(tmpdir, tags):
@@ -22,38 +37,19 @@ def init_treant(tmpdir, tags):
     return tf
 
 
-class TestTreantFile:
+def test_init_treant(tmpdir):
+    pool = mp.Pool(processes=4)
+    num = 73
 
-    @pytest.fixture
-    def treant(self, tmpdir):
-        with tmpdir.as_cwd():
-            t = Treant('sprout')
-        return t
+    # TODO: eventually want this to work without initing the treant
+    # here
+    init_treant(tmpdir, ['bark'])
+    for i in range(num):
+        pool.apply_async(init_treant, args=(tmpdir, ['run_{}'.format(i)]))
+    pool.close()
+    pool.join()
 
-    def test_death_by_1000_pokes(self, treant):
-        pool = mp.Pool(processes=4)
-        for i in range(10):
-            pool.apply_async(pokefile, args=(treant.abspath,
-                                             "run_{}".format(i)))
-        pool.close()
-        pool.join()
+    with tmpdir.as_cwd():
+        tf = Treant('sprout')
 
-        assert len(treant.tags) == 1000
-
-    def test_init_treant(self, tmpdir):
-        pool = mp.Pool(processes=4)
-        num = 73
-
-        # TODO: eventually want this to work without initing the treant
-        # here
-        init_treant(tmpdir, ['bark'])
-        for i in range(num):
-            pool.apply_async(init_treant, args=(tmpdir,
-                                                ['run_{}'.format(i)]))
-        pool.close()
-        pool.join()
-
-        with tmpdir.as_cwd():
-            tf = Treant('sprout')
-
-        assert len(tf.tags) == num + 1
+    assert len(tf.tags) == num + 1
