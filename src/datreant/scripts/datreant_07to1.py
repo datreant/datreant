292f110a709c6c7a6a96a601e52583f247566ab4
Max Linke <maxlinke88@gmail.com>
2018-06-12 19:33:01 +0200

    Add error handling and make script part of datreant

--- src/datreant/scripts/datreant_07to1.py
+++ src/datreant/scripts/datreant_07to1.py
@@ -1,19 +1,29 @@
 #! /usr/bin/env python
+import json
+import warnings
 from glob import glob
 from os import path
 
-import json
 import datreant
 
 
 def convert(folder):
-    json_file = glob(path.join(folder, 'Treant*'))[0]
+    treants = glob(path.join(folder, 'Treant*'))
+    if len(treants) == 0:
+        warnings.warn("No treant found in folder: {}".format(folder))
+        return
+    elif len(treants) > 1:
+        warnings.warn("Multiple treants found in folder: {}\n"
+                      "    Skipping conversion".format(folder))
+        return
+
+    json_file = treants[0]
     with open(json_file) as fh:
         old = json.load(fh)
     datreant.Treant(folder, categories=old['categories'], tags=old['tags'])
 
 
-if __name__ == '__main__':
+def main():
     import argparse
     parser = argparse.ArgumentParser(
         description="Convert pre 1.0 Treant to new Treant",
@@ -27,3 +37,7 @@ if __name__ == '__main__':
 
     for dir in args.directories:
         convert(dir)
+
+
+if __name__ == '__main__':
+    main()
