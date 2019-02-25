85cea93f532ff26fb2328d0c700948d545d6d35e
richardjgowers <richardjgowers@gmail.com>
2019-02-25 13:53:05 -0600

    discover and get tests

--- src/datreant/tests/test_cli.py
+++ src/datreant/tests/test_cli.py
@@ -45,14 +45,32 @@ def test_init(in_tmpdir, tags, categories):
 
 @pytest.fixture
 def readymade_treant(in_tmpdir):
-    t = dtr.Treant('duchamp',
-                   tags='art?',
+    t = dtr.Treant('duchamp/fountain',
+                   tags=['art?', 'duchamp'],
                    categories={'colour': 'white',
                                'material': 'porcelain'},
     )
 
+@pytest.fixture
+def readymades(readymade_treant):
+    t2 = dtr.Treant('duchamp')
+    t3 = dtr.Treant('duchamp/wheel',
+                    tags=['duchamp'],
+                    categories={'material': 'wood'})
+    t4 = dtr.Treant('manray')
+    t5 = dtr.Treant('manray/gift',
+                    tags=['manray'],
+                    categories={'material': 'metal',
+                                'spiky': True})
+    t6 = dtr.Treant('hausmann')
+    t7 = dtr.Treant('hausmann/head',
+                    tags=['hausmann'],
+                    categories={'material': 'wood',
+                                'spiky': False})
+
+
 def test_show(readymade_treant):
-    ret = subprocess.run('dtr show duchamp', shell=True,
+    ret = subprocess.run('dtr show duchamp/fountain', shell=True,
                          stdout=subprocess.PIPE,
                          stderr=subprocess.PIPE,
                          check=True)
@@ -61,3 +79,33 @@ def test_show(readymade_treant):
     assert 'art?' in output
     assert 'colour' in output
     assert 'white' in output
+
+
+def test_discover(readymades):
+    ret = subprocess.run('dtr discover', shell=True,
+                         stdout=subprocess.PIPE,
+                         stderr=subprocess.PIPE,
+                         check=True,
+    )
+
+    output = ret.stdout.decode()
+
+    print(output)
+    items = [val for val in output.split('\n')
+             if val]  # ignore blank lines
+    assert len(items) == 7
+
+
+def test_get(readymades):
+    ret = subprocess.run('dtr get -c material:wood', shell=True,
+                         stdout=subprocess.PIPE,
+                         stderr=subprocess.PIPE,
+                         check=True,
+    )
+
+    # should return 2 results, 'duchamp/wheel' and 'hausmann/head'
+    output = ret.stdout.decode()
+    print('#' + output + '#')
+    assert len(output.split('\n')) == 2
+    assert 'wheel' in output
+    assert 'head' in output
